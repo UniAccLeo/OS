3e11b57dba6e2a4866f519e0ea9d42124297813c
Leo Li <leojinshuoli@gmail.com>
2025-08-19 14:04:31 +0930

    fixed printing for even

--- even.c
+++ even.c
@@ -1,27 +1,27 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 
 void handleSignal(int sig) {
-  if(sig == SIGINT){
-    printf("Yeah!");
+  if (sig == SIGINT) {
+    printf("Yeah!\n");
   }
-  if(sig == SIGHUP) {
-    printf("Ouch!");
+  if (sig == SIGHUP) {
+    printf("Ouch!\n");
   }
 }
 
 int main(int argc, char *argv[]) {
-    signal(SIGINT, handleSignal);
-    signal(SIGHUP, handleSignal);
-    
-    int n = atoi(argv[1]);
-    for(int i = 0; i< n ; i++){
-      printf("%d", i*2);
-      fflush(stdout);
-      sleep(5);
-    }
+  signal(SIGINT, handleSignal);
+  signal(SIGHUP, handleSignal);
 
-    return 0;
+  int n = atoi(argv[1]);
+  for (int i = 0; i < n; i++) {
+    printf("%d\n", i * 2);
+    fflush(stdout);
+    sleep(5);
+  }
+
+  return 0;
 }
